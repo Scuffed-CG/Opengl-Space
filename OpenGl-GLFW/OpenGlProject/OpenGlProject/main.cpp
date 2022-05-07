6c784110ba4a83b056bacfa0b7f6385e76d328ff
Bernd U <berndu2002@gmail.com>
2022-05-07 18:42:43 +0200

    Update main.cpp

--- OpenGl-GLFW/OpenGlProject/OpenGlProject/main.cpp
+++ OpenGl-GLFW/OpenGlProject/OpenGlProject/main.cpp
@@ -1,4 +1,5 @@
 #include"Model.h"
+#include"Curve.h"
 #include <filesystem>
 
 const unsigned int width = 1920;
@@ -102,6 +103,48 @@ int main()
 	//Model backpack("IronMan/IronMan.obj");
 	//Model backpack("Survival_BackPack_2/Survival_BackPack_2.fbx");
 
+	//-----------------------------------------------------------------Testing zone-------------------------------------------------------------------
+
+	Curve curve1, curve2, curve3, curve4;
+
+	curve1.addControlPoint(0.0f, 0.0f, 0.0f);
+	curve1.addControlPoint(5.0f, 0.0f, 0.0f);
+	curve1.addControlPoint(10.0f, 0.0f, 15.0f);
+	curve1.addControlPoint(15.0f, 0.0f, 15.0f);
+
+	curve2.addControlPoint(15.0f, 0.0f, 15.0f);
+	curve2.addControlPoint(20.0f, 0.0f, 15.0f);
+	curve2.addControlPoint(5.0f, 0.0f, 30.0f);
+	curve2.addControlPoint(0.0f, 0.0f, 30.0f);
+
+	curve3.addControlPoint(0.0f, 0.0f, 30.0f);
+	curve3.addControlPoint(-5.0f, 0.0f, 30.0f);
+	curve3.addControlPoint(-10.0f, 0.0f, 15.0f);
+	curve3.addControlPoint(-15.0f, 0.0f, 15.0f);
+
+	curve4.addControlPoint(-15.0f, 0.0f, 15.0f);
+	curve4.addControlPoint(-20.0f, 0.0f, 15.0f);
+	curve4.addControlPoint(-5.0f, 0.0f, 0.0f);
+	curve4.addControlPoint(0.0f, 0.0f, 0.0f);
+
+
+	// Vul 1 vector met alle punten
+	std::vector<glm::vec3> curvePoints = curve1.getCurve();
+	std::vector<glm::vec3> tempPoints;
+	tempPoints = curve2.getCurve();
+	for (int i = 1; i < tempPoints.size(); ++i) {
+		curvePoints.push_back(tempPoints[i]);
+	}
+	tempPoints = curve3.getCurve();
+	for (int i = 1; i < tempPoints.size(); ++i) {
+		curvePoints.push_back(tempPoints[i]);
+	}
+	tempPoints = curve4.getCurve();
+	for (int i = 1; i < tempPoints.size() - 1; ++i) {
+		curvePoints.push_back(tempPoints[i]);
+	}
+
+	//-----------------------------------------------------------------End of Testing zone------------------------------------------------------------
 
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_CULL_FACE);
@@ -114,7 +157,15 @@ int main()
 	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 	double prev_time = glfwGetTime();
-	
+
+	//Temp vars
+	double prev_time_camera = glfwGetTime();
+	double prev_time_backpack = glfwGetTime();
+	glm::vec3 trans = glm::vec3(0.0f, 0.0f, 0.0f);
+	bool moveCamera = false;
+	bool moveBackpack = false;
+	int currentPoint = 0;
+	//End Temp vars
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -122,14 +173,48 @@ int main()
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		if (curr_time - prev_time  >= (double)(1 / 60)) {
+		if (curr_time - prev_time >= (double)(1 / 60)) {
 			camera.Inputs(window);
+			prev_time = glfwGetTime();
 		}
+
+
+		//-----------------------------------------------------------------Testing zone-------------------------------------------------------------------
+		if (moveCamera)
+			if (curr_time - prev_time_camera >= (double)(0.05f)) {
+				camera.setPosition(curvePoints[currentPoint]);
+				++currentPoint;
+				if (!(currentPoint < curvePoints.size()))
+					currentPoint = 0;
+				prev_time_camera = glfwGetTime();
+			}
+		//-----------------------------------------------------------------End of Testing zone------------------------------------------------------------
+
+
 		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
 		//suit.Draw(shaderProgram, camera);
 		glCullFace(GL_BACK);
-		backpack.Draw(shaderProgram, camera);
+
+
+		//-----------------------------------------------------------------Testing zone-------------------------------------------------------------------
+		if (moveBackpack) {
+			if (curr_time - prev_time_backpack >= (double)(0.05f)) {
+				trans = curvePoints[currentPoint];
+				++currentPoint;
+				if (!(currentPoint < curvePoints.size()))
+					currentPoint = 0;
+				prev_time_backpack = glfwGetTime();
+			}
+		}
+		else {
+			trans = glm::vec3(0.0f, 0.0f, 0.0f);
+		}
+
+		//-----------------------------------------------------------------End of Testing zone------------------------------------------------------------
+
+
+		backpack.Draw(shaderProgram, camera, trans);
 		glCullFace(GL_FRONT);
 
 		glfwSwapBuffers(window);
